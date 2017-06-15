c285d242052c54aca66d88b9fc744d87df4b8889
Ian Duke <ian.c.duke@gmail.com>
2017-06-15 15:14:55 -0400

    Refactored code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,55 +11,78 @@ $(document).ready(function () {
   }
 
   // handle the new tweets
-    $(".new-tweet").on("submit", function(event) {
-      event.preventDefault();
-      $(this).serialize();
-    })
+  $(".new-tweet").on("submit", function(event) {
+    event.preventDefault();
+    $(this).serialize();
+  })
 
-  function loadTweets() {
-    //  fetch tweets from http://localhost:8080/tweets
-    //  use jQuery to request /tweets and receive the array as JSON
-    $.ajax({
-      method: 'GET',
-      url: '/tweets',
-      dataType: 'JSON',
-      success: function (tweetData) {
-        renderTweets(tweetData);
-      },
-      fail: (err) => {
-        console.debug('Something blew up', err);
-      }
-    });
-  }
 
-  function renderTweets(tweets) {
-    for (var i = 0; i < tweets.length; i++) {
-      $("#tweetList").append(createTweetElement(tweets[i]))
+  /**
+  @description: error handler
+  @params:
+          method and error in question
+  */
+  const handleError = (method) => {
+    return (err) => {
+      console.debug(method, err);
     }
   }
 
-  function createTweetElement(tweet) {
-    var timeElapsed = Math.floor((Date.now() - tweet.created_at)/(1000*60*60*24));
+  /**
+  @description: fetches tweets from http://localhost:8080/tweets
+  @params:
+          cb for when tweets are loaded
+  */
 
-    var $tweet = $('<article>').addClass('tweet')
-    .append(
-      $("<header>")
-      .append($("<img>").addClass('usericon').attr("src", tweet.user.avatars.small))
-      .append($("<span>").addClass('usersname').text(tweet.user.name))
-      .append($("<span>").addClass('userid floatright').text(tweet.user.handle))
-    )
-    .append(
-      $("<div>").addClass("tweetbody")
-      .append($("<p>").text(tweet.content.text))
-    )
-    .append(
-      $("<footer>")
-      .append($("<span>").text(timeElapsed + ' days ago'))
-      .append($("<span>").addClass("icons floatright").html('<i class="fa fa-heart fa-lg"></i><i class="fa fa-retweet fa-lg"></i><i class="fa fa-flag fa-lg"></i>'))
-    );
-    return $tweet;
+  const loadTweets = (cb) => {
+    $.getJSON({
+      url: '/tweets',
+      success: (response) => {
+        cb(response);
+      },
+      fail: handleError('loadTweets')
+    })
   }
 
-  loadTweets();
-  renderTweets(tweetData);
+
+  // @description: converts each tweet into HTML string and appends to page
+  const renderTweets = (data) => {
+    const tweet_feed = data.map((tweet) => {
+      var timeElapsed = Math.floor((Date.now() - tweet.created_at)/(1000*60*60*24));
+      return `
+        <article class="tweet">
+          <header>
+              <span class="usericon">
+              <img src="${tweet.user.avatars.small}">
+              </span>
+            <span class="usersname">
+              ${tweet.user.name}
+            </span>
+            <span class="userid floatright">
+              ${tweet.user.handle}
+            </span>
+          </header>
+          <div class="tweetbody">
+            ${tweet.content.text}
+          </div>
+            <footer>
+              <span class="whatevs">
+                ${timeElapsed} days ago
+              </span>
+              <span class="icons floatright">
+                <i class="fa fa-heart fa-lg"></i>
+                <i class="fa fa-retweet fa-lg"></i>
+                <i class="fa fa-flag fa-lg"></i>
+              </span>
+            </footer>
+        </article>`
+    })
+    $("#tweetList").append(tweet_feed.join(''));
+  }
+
+  // on page load
+  loadTweets(renderTweets);
+
+  // eventually, add a click listener (handler) to load more content on('click')
+
 });
