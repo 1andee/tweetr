c38799ae8b2fd8fab145da9b66486e5a4097c863
Ian Duke <ian.c.duke@gmail.com>
2017-06-17 23:24:12 +0000

    Updated comments

--- public/scripts/timeElapsed.js
+++ public/scripts/timeElapsed.js
@@ -1,19 +1,24 @@
 /**
 @description: displays elapsed time from tweet creation
-@params: timestamp, which is the unix time each tweet was created
+              nested if statements return singular 'minute/hour/day' versus plural
+@params: timestamp, which is the unix time of when each tweet was created
 */
 
 const timeElapsed = (timestamp) => {
   let difference =  ((Date.now() - timestamp) / 1000);
   let year = 31557600;
 
+  // Less than 29 seconds
   if (difference < 29) {
     return `now`
   }
+  // Returns # of seconds ago
   if (difference < 59) {
     var secondsAgo = Math.floor(difference);
     return `${secondsAgo} seconds ago`
   }
+
+  // Returns # of minutes ago
   if (difference < 3599) {
     var minutesAgo = Math.floor(difference/60);
       if (minutesAgo < 2) {
@@ -21,6 +26,8 @@ const timeElapsed = (timestamp) => {
       }
     return `${minutesAgo} minutes ago`
   }
+
+  // Returns # of hours ago
   if (difference < 86399) {
     var hoursAgo = Math.floor(difference/3600);
       if (hoursAgo < 2) {
@@ -28,6 +35,8 @@ const timeElapsed = (timestamp) => {
       }
     return `${hoursAgo} hours ago`
   }
+
+  // Returns # of days ago
   if (difference < (year - 86400)) {
     var daysAgo = Math.floor(difference/86400);
       if (daysAgo < 2) {
@@ -35,6 +44,8 @@ const timeElapsed = (timestamp) => {
       }
     return `${daysAgo} days ago`
   }
+
+  // Returns # of years ago
   if (difference > (year - 86400)) {
     var yearsAgo = Math.floor(difference/year);
       if (yearsAgo < 2) {
