7cb146556955eadfd692a0fe0a15146c3bc28c94
Ian Duke <ian.c.duke@gmail.com>
2017-08-07 01:45:39 -0400

    Cleaning up / removing commented-out code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,12 @@
 $(document).ready(function () {
 
-
-  // /**
-  // @description: checks to see if user is logged in
-  // */
-  // const userLoginState = () => {
-  //   if (!req.session.user_id) {
-  //     let response = $("#navbar").append(renderLoginButtons());
-  //   }
-  // }
-  //
-  // /**
-  // @description: renders buttons if user is not logged in
-  // */
-  // const renderLoginButtons = () => {
-  //   return `
-  //   <span class="floatright"><button value="Login">Login</span>
-  //   <span class="floatright"><button value="Register">Register</span>`
-  //   // <span class="floatright"><button value="Logout">Logout</span>
-  // }
-
   // slider animation for compose button
   $("#compose").on("click", () => {
     $(".new-tweet").slideToggle();
     $(".new-tweet textarea").focus();
   })
 
+  // form post for logout button
   $("#logout").on("click", () => {
     $.post({
       url: '/users/logout',
